276fc86008789cd324bf945794715a4722537ec2
Bard <brad50916@gmail.com>
2024-04-10 15:27:43 -0500

    test

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 
 // get the port from env variable
+// test comment1234
 const PORT = process.env.PORT || 5000;
 
 app.use(express.static("dist"));
