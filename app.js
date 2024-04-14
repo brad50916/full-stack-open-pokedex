b7f598d1ba00a156ce1e6cc28c7e12da3d5bcb27
Bard <brad50916@gmail.com>
2024-04-14 15:34:12 -0500

    build

--- app.js
+++ app.js
@@ -11,6 +11,6 @@ app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)
 })
 
-// app.get('/version', (req, res) => {
-//   res.send('1') // change this string to ensure a new version deployed
-// })
+app.get('/version', (req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
+})
